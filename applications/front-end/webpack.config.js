3900db62b68c81bd8c57674d7d5b2b42559fc2c0
Eno Compton <ecompton@pivotal.io>
2016-07-25 16:21:06 +0900

    Add front-end as part of build process
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

---
+++ applications/front-end/webpack.config.js
@@ -0,0 +1,21 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+      path: __dirname + '/build',
+      filename: 'bundle.js'
+  },
+  module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel'
+        }
+      ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "src/index.html"
+  })]
+};
---
+++ applications/front-end/webpack.dist.config.js
@@ -0,0 +1,13 @@
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
+config.plugins = config.plugins.concat([
+  new webpack.DefinePlugin({
+    'process.env':{
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin()
+]);
+
+module.exports = config;
